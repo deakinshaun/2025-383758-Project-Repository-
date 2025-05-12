6932f2f3f61f9da42ca5e6d9402ccdffa016a279
JasonStarkGit <61034990+GitProGamer@users.noreply.github.com>
2025-05-13 01:08:46 +1000

    MainScene Merged
    Hey guys, I have merged the BodyTrack System with the BasketBall scene together, I have comment out the script"TypingArea" as it keep preventing me from build and test however not a single line of code has been deleted, please let me know if you have any questions or need any help, Thank you!

--- TeamB_Project/Paralympic_Games/Assets/LewyFolder/Scripts/TypingArea.cs
+++ TeamB_Project/Paralympic_Games/Assets/LewyFolder/Scripts/TypingArea.cs
@@ -1,46 +1,46 @@
-using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-using UnityEditor.Experimental.GraphView;
+// using UnityEngine;
+// using System.Collections.Generic;
+// using System.Collections;
+// using UnityEditor.Experimental.GraphView;
 
-public class TypingArea : MonoBehaviour
-{
+// public class TypingArea : MonoBehaviour
+// {
 
-    public GameObject leftHand;
-    public GameObject RightHand;
-    public GameObject leftTypingHand;
-    public GameObject RightTypingHand;
+//     public GameObject leftHand;
+//     public GameObject RightHand;
+//     public GameObject leftTypingHand;
+//     public GameObject RightTypingHand;
    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnTriggerEnter(Collider other)
-    {
-        GameObject hand = other.GetComponent<OVRGrabber>().gameObject;
-        if (hand == null) return;
-        if (hand == leftHand)
-        {
-            leftTypingHand.SetActive(true);
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         GameObject hand = other.GetComponent<OVRGrabber>().gameObject;
+//         if (hand == null) return;
+//         if (hand == leftHand)
+//         {
+//             leftTypingHand.SetActive(true);
 
-        }
-        else if (hand == RightHand)
-        {
-            RightTypingHand.SetActive(true);
-        }
+//         }
+//         else if (hand == RightHand)
+//         {
+//             RightTypingHand.SetActive(true);
+//         }
         
-    }
+//     }
 
-    // Update is called once per frame
-    private void OnTriggerExit(Collider other)
-    {
-        GameObject hand = other.GetComponent<OVRGrabber>().gameObject;
-        if (hand == null) return;
-        if (hand == leftHand)
-        {
-            leftTypingHand.SetActive(false);
+//     // Update is called once per frame
+//     private void OnTriggerExit(Collider other)
+//     {
+//         GameObject hand = other.GetComponent<OVRGrabber>().gameObject;
+//         if (hand == null) return;
+//         if (hand == leftHand)
+//         {
+//             leftTypingHand.SetActive(false);
 
-        }
-        else if (hand == RightHand)
-        {
-            RightTypingHand.SetActive(false);
-        }
-    }
-}
+//         }
+//         else if (hand == RightHand)
+//         {
+//             RightTypingHand.SetActive(false);
+//         }
+//     }
+// }
