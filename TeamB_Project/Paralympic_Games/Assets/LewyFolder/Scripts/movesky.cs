aae55d477f0be460c54bfd3f972b34e5f586795b
lewysflies <59502949+lewysflies@users.noreply.github.com>
2025-05-18 15:27:26 +1000

    added clouds in main scene

---
+++ TeamB_Project/Paralympic_Games/Assets/LewyFolder/Scripts/movesky.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class movesky : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 1.5f);
+    }
+}
